2f598bf252b699e310e569af4adc6b160de86a0c
Lin <u1062248@gmail.com>
2026-03-07 17:19:59 +0800

    feat: 新增聊天消息、課程反饋和評價的控制器、服務和資料庫存取層

---
+++ src/main/java/com/learning/api/repo/BookingRepository.java
@@ -0,0 +1,8 @@
+package com.learning.api.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.learning.api.entity.Booking;
+
+
+public interface BookingRepository extends JpaRepository<Booking, Long> {
+}
